0d0da5ecbd8c94066e7ea85509bab3d3b399acc9
jess <jm219137@falmouth.ac.uk>
2019-09-19 15:53:55 +0100

    files attached

---
+++ 3rd year project/Assets/Scripts/CamMouse.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMouse : MonoBehaviour
+{
+
+    Vector2 mouseLook;
+    Vector2 smoothV;
+    public float sensitivity = 5.0f;
+    public float smoothing = 2.0f;
+
+
+
+
+    GameObject Player;
+
+    // Use this for initialization
+    void Start()
+    {
+        Player = this.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+       
+
+
+
+    }
+}
